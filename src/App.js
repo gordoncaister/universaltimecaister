3a69e672e07235123942c2263f0f48795125c2db
Gordon Caister <gordoncaister@gmail.com>
2020-06-25 19:43:47 -0500

    added time picker

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
 import React, { useState, useEffect } from 'react';
-
+import TimePicker from 'react-time-picker'; 
 
 import './App.css';
 import moment from 'moment';
 import {
   Card, CardImg, Jumbotron, CardBody,
-  CardTitle, CardSubtitle, Container, Row, Col
+  CardTitle, CardSubtitle, Container, Row, Col, Dropdown, DropdownItem, DropdownToggle, DropdownMenu, CardText
 } from 'reactstrap';
-
+let convertedTime = moment(new Date()).zone("+02:00").format("LTS")
 function App() {
 
-  let [date, setDate] = useState(new Date())
+  const [date, setDate] = useState(new Date())
+  const [dropdownLocationOpen, setDropdownLocationOpen] = useState(false);
+  const [selectTime, setSelectTime] = useState(date);
+  const [country, setCountry] = useState("South Africa")
 
-  const updateDate = () => {
-    setDate(new Date())
+  const toggleLocation = () => setDropdownLocationOpen(prevState => !prevState);
+
+  const updateCountry = (value) => {
+    setCountry(value)
   }
 
+  const updateSelectTime = (value) => {
+    console.log(value)
+    setSelectTime(value)
+  }
+
+
+
   useEffect(()=> {
-    setInterval(updateDate(),1000)
-  })
+    setInterval(() => setDate(new Date()),1000)
+    if(country === "USA"){
+      convertedTime = moment(selectTime,"hh:mm:ss").zone("-05:00").format("LTS")
+    }
+    if(country === "UK"){
+      convertedTime = moment(selectTime,"hh:mm:ss").zone("+01:00").format("LTS")
+    }
+    if(country === "SA"){
+      convertedTime = moment(selectTime,"hh:mm:ss").zone("+02:00").format("LTS")
+    }
+  },[country, selectTime])
 
   return (
     <div className="App">
@@ -29,7 +50,7 @@ function App() {
         </Container>
       </Jumbotron>
       <Container>
-        <Row Row xs="1" sm="3" md="3" lg="3">
+        <Row xs="1" sm="3" md="3" lg="3">
           <Col>
             <Card>
               <CardImg top width="100%" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Flag_of_South_Africa.svg/1200px-Flag_of_South_Africa.svg.png" alt="Flag of South Africa.svg"/>
@@ -58,6 +79,40 @@ function App() {
             </Card>
             </Col>
         </Row>
+      </Container>
+      <Container xs="1" sm="1" md="1" lg="1">
+        <Card>
+          <CardBody>
+            <CardTitle>What time is it in? </CardTitle>
+            <Dropdown isOpen={dropdownLocationOpen} toggle={toggleLocation}>
+              <DropdownToggle caret>
+                {country}
+              </DropdownToggle>
+              <DropdownMenu>
+                <DropdownItem onClick={() => updateCountry("UK")}>UK</DropdownItem>
+                <DropdownItem onClick={() => updateCountry("USA")}>Murica</DropdownItem>
+                <DropdownItem onClick={() => updateCountry("SA")}>South Africa</DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
+            <br/>
+            <CardSubtitle>When it is <TimePicker
+              amPmAriaLabel="Select AM/PM"
+              clearAriaLabel="Clear value"
+              hourAriaLabel="Hour"
+              maxDetail="second"
+              minuteAriaLabel="Minute"
+              nativeInputAriaLabel="Time"
+              onChange={updateSelectTime}
+              secondAriaLabel="Second"
+              value={selectTime}
+              disableClock="true"
+            />?</CardSubtitle>
+            <CardText>It is: <strong>{convertedTime}</strong> in {country}</CardText>
+            <CardText>
+              
+            </CardText>
+          </CardBody>
+        </Card>
       </Container>
     </div>
   );
